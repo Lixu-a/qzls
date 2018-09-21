ce723e0a2ed967c7f764bb2d906c26c78e066f16
Lixu-a <1063040087@qq.com>
2018-09-21 14:11:06 +0800

    天气

--- qzls/qzls/components/app-news/app-news.js
+++ qzls/qzls/components/app-news/app-news.js
@@ -1,4 +1,5 @@
 import { PublicCmsFinder } from '../../services/public/finder/public-cms-finder'
+var bmap = require('./bmap-wx.min.js');
 
 const app = getApp()
 
@@ -14,7 +15,8 @@ Component({
     data: {
         height: 464,
         initHeight: false,
-        showVideo: false
+        showVideo: false,
+        weatherData: '泉州市 今日阴转多云'
     },
 
     attached: function(){},
@@ -22,7 +24,35 @@ Component({
         publicCmsFinder.getCmsTitles(app.services.constantService.CACHE_READ_WRITE)
             .subscribe((res)=>{
                 this.setData({list: res.data.list})
-            })
+            });
+            console.log("BMap调用成功");
+            var that = this;
+            // 新建百度地图对象 
+            var BMap = new bmap.BMapWX({
+              ak: '4DKdZcq5G7Go4RYLXmZpTPgSCtbAQInE'
+            });
+            var fail = function (data) {
+              console.log(data)
+            };
+            var success = function (data) {
+              var weatherData = data.currentWeather[0];
+              // console.log("11111111");
+              // console.log(weatherData);
+              // var time = new Date().toISOString();
+              weatherData = weatherData.currentCity + " " + weatherData.date;
+              // var weather = { cmsuid:  null,  createtimestamp: time,  title:  title};
+              // var list = that.data.list.push(weather);
+              // console.log(list);
+              // console.log(that.data.list.push(weather));
+              that.setData({
+                weatherData: weatherData
+              });
+            }
+            // 发起weather请求 
+            BMap.weather({
+              fail: fail,
+              success: success
+            });
     },
     moved: function(){},
     detached: function(){},
