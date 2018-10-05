bbce157350aeaa3f9a2ddcb7568ddb0a8469f13f
Lixu-a <1063040087@qq.com>
2018-10-05 18:27:23 +0800

    适配

--- qzlspc/src/components/headerNav.vue
+++ qzlspc/src/components/headerNav.vue
@@ -1,26 +1,28 @@
   <template>
 	<div class="headerNav">
-		<el-menu
-		:default-active="$route.path"
-		class="el-menu-demo"
-		mode="horizontal"
-		@select="handleSelect"
-		background-color="#fff"
-		text-color="#000"
-		active-text-color="#FE701A"
-		router>
-		<!-- #F7F7F7 -->
-			<el-menu-item index="/home">首页</el-menu-item>
-			<el-menu-item index="/building">楼盘</el-menu-item>
-			<el-menu-item index="/news">热点资讯</el-menu-item>
-		</el-menu>
-		<div class="logo">
-			<router-link to="/home">
-				<img src="../assets/images/logo.png" alt="">
-			</router-link>
-			<router-link to="/home">
-				<div class="title">泉州楼市</div>
-			</router-link>
+		<div class="public-container" style="position:relative;">
+			<el-menu
+			:default-active="$route.path"
+			class="el-menu-demo"
+			mode="horizontal"
+			@select="handleSelect"
+			background-color="#fff"
+			text-color="#000"
+			active-text-color="#FE701A"
+			router>
+			<!-- #F7F7F7 -->
+				<el-menu-item index="/home">首页</el-menu-item>
+				<el-menu-item index="/building">楼盘</el-menu-item>
+				<el-menu-item index="/news">热点资讯</el-menu-item>
+			</el-menu>
+			<div class="logo">
+				<router-link to="/home">
+					<img src="../assets/images/logo.png" alt="">
+				</router-link>
+				<router-link to="/home">
+					<div class="title">泉州楼市</div>
+				</router-link>
+			</div>
 		</div>
 	</div>
 </template>
@@ -46,11 +48,16 @@
 <style scoped>
 	.headerNav{
 		position: relative;
+		width: 100%;
+		background-color: #fff;
+		box-shadow: 0px 0px 10px rgba(0,0,0,.3);
+		height: 60px;
 	}
 	.el-menu-demo {
-		box-shadow: 0px 0px 10px rgba(0,0,0,.3);
-		min-width: 1200px;
-		position: relative;
+		margin: 0 auto;
+		position: absolute;
+		left: 200px;
+		top: 0;
 	}
 	.el-menu-demo .el-menu-item{
 		font-size: 18px;
@@ -61,8 +68,8 @@
 	}
 	.logo{
 		position: absolute;
-		left: 360px;
-		top: 0px;
+		left: 0;
+		top: 0;
 		display: flex;
 	}
 	.logo img{
